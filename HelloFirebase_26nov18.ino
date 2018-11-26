e5fb0cbe46fcf861bd063a65acee890da51a6bb9
masterUNG <phrombutr@gmail.com>
2018-11-26 11:44:23 +0700

    Conected Router

--- HelloFirebase_26nov18.ino
+++ HelloFirebase_26nov18.ino
@@ -1,7 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
-}
+
+//Config Connected Router
+#define WiFi_SSID "MastreEWTC_2.4G"
+#define WiFi_Password "12345abcde"
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+
+//  Connected Router
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WiFi_SSID, WiFi_Password);
+  Serial.print("Conecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+    }
+
+    Serial.println();
+    Serial.print("Success Connected : ");
+    Serial.println(WiFi.localIP());
+
+    
+
+  
+
+} // setup
 
 void loop() {
   // put your main code here, to run repeatedly:
